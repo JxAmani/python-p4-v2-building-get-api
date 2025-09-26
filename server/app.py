19c1592980707f1d3cc3f31b8f82297b71acd9ab
JxAmani <jeremyamani336@gmail.com>
2025-09-27 00:13:52 +0300

    Update

--- server/app.py
+++ server/app.py
@@ -19,9 +19,37 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
-# start building your API here
+# ------------------------
+#        ROUTES
+# ------------------------
+
+# Get all games
+@app.route('/games')
+def games():
+    games = Game.query.all()
+    return make_response(
+        jsonify([game.to_dict() for game in games]),
+        200
+    )
+
+# Get one game by id (with reviews + users)
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    if not game:
+        return make_response({"error": "Game not found"}, 404)
+    return make_response(game.to_dict(), 200)
+
+# Get all users who reviewed a given game
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    if not game:
+        return make_response({"error": "Game not found"}, 404)
+
+    users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+    return make_response(users, 200)
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
